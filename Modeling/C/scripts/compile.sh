2395eac9404ba178f35676e0a2a5e4fb932f6bea
Carlos Olaya <carlos.olaya.reyes@gmail.com>
2019-11-06 00:07:13 -0300

    Modeling C Makefile Ok Last Petalinux Image 110319

--- Modeling/C/scripts/compile.sh
+++ Modeling/C/scripts/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-C_ROOT=$HOME/hexapod/Modeling/C/
+C_ROOT=$HOME/hexapod_project/Modeling/C/
 C_INC=$C_ROOT/includes/
 C_EXE=$C_ROOT/outputs/
 
@@ -16,3 +16,4 @@ gcc -c -o $C_EXE/hexapod_gaits.o -I$C_INC $C_INC/hexapod_gaits.c
 gcc -c -o $C_EXE/ikinematics_gait_test.o -I$C_INC $C_ROOT/ikinematics_gait_test.c
 gcc -o $C_EXE/ikinematics_gait_test $C_EXE/axi_ikinematics.o $C_EXE/hexapod_gaits.o $C_EXE/ikinematics_gait_test.o
 
+rm $C_EXE/*.o
--- Petalinux/Images/README.md
+++ Petalinux/Images/README.md
@@ -1,3 +1,3 @@
 The rootfs.ext4 (2GB~) is in the following drive:
 
-https://drive.google.com/open?id=11_BRXsqsCObvRZ0UUeoS6SobJCpwvsEd
+https://drive.google.com/open?id=1hqdLbtV3BzI2Pu3ZI251gpTROWYbXEXu
