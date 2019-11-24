1c3ccb5c6a6c74c5d2da9afc91638de4c5a1d479
colayare <carlos.olaya.reyes@gmail.com>
2019-11-23 23:42:08 -0300

    Added function set_initial_positions, it will be appended to startup script

---
+++ Modeling/Python/set_initial_position.py
@@ -0,0 +1,26 @@
+import os
+import sys
+#### Append includes path           
+pathname = os.path.dirname(sys.argv[0])   
+abs_path = os.path.abspath(pathname)
+sys.path.append(abs_path+'/includes')
+from hexapod_class import hexapod_kinematics as hc
+
+#### Params Files
+joint_offsets_filename  = abs_path+"/params/joint_offset.params"
+gait_steps_filename     = abs_path+'/params/gait_steps.params'
+init_position_filename  = abs_path+'/params/init_position.params'
+
+#### Code
+## Initialize Hexapod Class & Memory Map
+hexapod = hc()
+hexapod.init_axi()
+
+## Import Parameters
+hexapod.offsets_file = joint_offsets_filename
+hexapod.gait_steps_file = gait_steps_filename
+hexapod.init_position_file = init_position_filename
+hexapod.import_init_pos()
+hexapod.import_offsets()
+hexapod.set_default_offsets()
+hexapod.set_init_position()
