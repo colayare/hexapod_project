c699c2684df1564b629cafbc9a6d6ef8bfb8313e
colayare <carlos.olaya.reyes@gmail.com>
2020-04-11 15:17:52 -0300

    Updated set init position

--- Modeling/Python/set_initial_position.py
+++ Modeling/Python/set_initial_position.py
@@ -1,25 +1,28 @@
 import os
 import sys
-#### Append includes path           
+
+###############################################################################
+#### Append Paths 
+###############################################################################
 pathname = os.path.dirname(sys.argv[0])   
 abs_path = os.path.abspath(pathname)
+print('Appending path'+ abs_path+'/includes')
 sys.path.append(abs_path+'/includes')
-from hexapod_class import hexapod_kinematics as hc
+from hexapod_class import hexapod_kinematics as hexapod
 
-#### Params Files
-joint_offsets_filename  = abs_path+"/params/joint_offset.params"
-gait_steps_filename     = abs_path+'/params/gait_steps.params'
-init_position_filename  = abs_path+'/params/init_position.params'
-
-#### Code
-## Initialize Hexapod Class & Memory Map
-hexapod = hc()
+###############################################################################
+#### Hexapod Object
+###############################################################################
+hexapod = hexapod(invoke_axi_ip=True)
 
 ## Import Parameters
-hexapod.offsets_file = joint_offsets_filename
-hexapod.gait_steps_file = gait_steps_filename
-hexapod.init_position_file = init_position_filename
+hexapod.offsets_file_path            = abs_path+'/params/joint_offset.params'
+hexapod.init_position_file_path      = abs_path+'/params/init_position.params'
+hexapod.init_servo_inv_file_path     = abs_path+'/params/init_servo_inv.params'
+
+## Initialize Parameters
 hexapod.import_init_pos()
 hexapod.import_offsets()
-hexapod.set_default_offsets()
-hexapod.set_init_position()
+hexapod.import_init_servo_invertion()
+hexapod.set_default_offsets(print_out=False)
+hexapod.set_init_position(print_out=False)
