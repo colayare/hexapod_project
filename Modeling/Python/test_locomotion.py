24b15941927718cb1a7e4603b32c1866589f9a8b
colayare <carlos.olaya.reyes@gmail.com>
2020-04-14 00:53:06 -0300

    Updated locomotion

--- Modeling/Python/test_locomotion.py
+++ Modeling/Python/test_locomotion.py
@@ -45,7 +45,7 @@ angle           = float(input('Insert Angle(sexa) = '))
 alf             = angle * pi / 180
 
 ## Gait
-walk            = 1
+walk            = input('Insert Walk [1-4] = ')
 
 ###############################################################################
 #### Traslational Locomotion
