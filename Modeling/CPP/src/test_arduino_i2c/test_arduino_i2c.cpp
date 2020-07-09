b968ea8038d8316f0a4c9e777ce019a8391822d1
Roy Olaya <carlos.olaya.reyes@gmail.com>
2020-07-09 20:43:33 -0300

    testing

--- Modeling/CPP/src/test_arduino_i2c/test_arduino_i2c.cpp
+++ Modeling/CPP/src/test_arduino_i2c/test_arduino_i2c.cpp
@@ -15,9 +15,14 @@ int main(void) {
   i2c_ip_context arduino("/dev/i2c-0");
   arduino.set_address(ARDUINO_I2C_ADDR);
 
-  // Test
+  // Test Write
   arduino.i2c_write_byte(0x0A);
   arduino.i2c_write_byte(0x55);
+
+  // Test Read
+  uint16_t read_val;
+  read_val = arduino.i2c_read_word_data(0x00);
+  cout << "Read : " << hex << read_val << endl;
 
   return 0;
 }
